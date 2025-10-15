cc733ba5aad3c0608dfe4bd71f6c02dd870e6a65
Hammad Masood <hammadmasood347@gmail.com>
2025-10-15 22:06:39 +0500

    New Table file added

---
+++ Table - Copy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, tab;
+    cout << "Enter Number for table :" << endl;
+    cin >> num;
+
+    for (int i = 1; i <= 10; i++) {
+        tab = num * i;
+        cout << num << " * " << i << " = " << tab << endl;
+    }
+    return 0;
+}
